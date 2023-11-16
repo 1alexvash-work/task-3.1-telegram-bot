f152b1a0fcf9acfbe2d0e1e9afd3e2da403ecd25
Alexander Vashchuk <1alexvash.work@gmail.com>
2023-11-16 11:32:26 +0200

    feat: printing out telegramBotToken in addition

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
-import fs from "fs";
+import "dotenv/config";
 
 console.log("Hello world!");
+
+const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+console.log("telegramBotToken:", telegramBotToken);
