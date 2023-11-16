57251bc1f197f0878e28bcebd2dd5eb140179613
Alexander Vashchuk <1alexvash.work@gmail.com>
2023-11-16 11:51:48 +0200

    feat: add an example from Bing
    Damn, it's working hooray!

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,21 @@ console.log("Hello world!");
 
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
 console.log("telegramBotToken:", telegramBotToken);
+
+// Bing example
+
+import TelegramBot from "node-telegram-bot-api";
+const token = telegramBotToken!;
+const bot = new TelegramBot(token, {
+  polling: true,
+});
+
+// Listen for any kind of message
+bot.on("message", (msg) => {
+  console.log(msg);
+});
+
+// Listen for when a user joins the chat
+bot.on("new_chat_members", (msg) => {
+  console.log(msg);
+});
