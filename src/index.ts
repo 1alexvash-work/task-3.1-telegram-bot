812f95e272283457c15b5179c6c839a0342634f6
Alexander Vashchuk <1alexvash.aiscout@gmail.com>
2023-11-17 00:29:33 +0200

    feat: simple http server

--- src/index.ts
+++ src/index.ts
@@ -19,3 +19,15 @@ bot.on("message", (message) => {
 bot.on("new_chat_members", (message) => {
   console.log(message);
 });
+
+// set http server on the port 3000
+import http from "http";
+
+const port = process.env.PORT || 3000;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
+  })
+  .listen(port);
