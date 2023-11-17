769e9ef91846e76dd19fed5d7d4dbbb51b621e78
Alexander Vashchuk <1alexvash.aiscout@gmail.com>
2023-11-17 12:20:56 +0200

    feat: add project deploy command
    This is a bit suboptimal approach due to this warning:  Due to `builds` existing in your configuration file, the Build and Development Settings defined in your Project Settings will not apply.
    
    While Vercel isn't inherently designed for running NodeJS, I've opted to retain this flow temporarily, acknowledging its limitations for now.

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,6 @@ const port = process.env.PORT || 3000;
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
+    res.end("Hello World!");
   })
   .listen(port);
