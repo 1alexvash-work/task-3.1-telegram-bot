fc0500cf027754634799a14664dbbf08155df9d8
Alexander Vashchuk <1alexvash.aiscout@gmail.com>
2023-11-18 16:57:30 +0200

    refactor: clean Cats model

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,6 @@ app.listen(port, () => {
 
 // create mongoose connection to MongoDB
 import mongoose from "mongoose";
-import Cat from "./models/Cat.js";
 
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ovflcai.mongodb.net/`;
 
@@ -45,17 +44,6 @@ try {
   console.log("MongoDB connected successfully");
 } catch (error) {
   console.error("MongoDB connection error:", error);
-}
-
-const kitty = new Cat({
-  name: "Nice Kitty",
-});
-
-try {
-  await kitty.save();
-  console.log("meow");
-} catch (error) {
-  console.log("error:", error);
 }
 
 // Todos
